15867c4a4a0c62c9d4b4f4d71c466017f8373038
Paul Sohier <paul999@phpbb.com>
2015-05-29 15:41:07 +0200

    Fix classname as well

--- DependencyInjection/phpBBSessionsAuthExtension.php
+++ DependencyInjection/phpBBSessionsAuthExtension.php
@@ -20,7 +20,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class phpBBphpBBSessionsAuthExtension extends Extension
+class phpBBSessionsAuthExtension extends Extension
 {
     /**
      * {@inheritDoc}
