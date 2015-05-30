6fa12570494dd145277d937d1b15a7e8eefea550
Paul Sohier <paul999@phpbb.com>
2015-05-30 12:02:15 +0200

    Add Subscriber to update the prefix for the users table.

---
+++ Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @package phpBBSessionsAuthBundle
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license MIT
+ *
+ */
+namespace phpbb\SessionsAuthBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @package phpbb\SessionsAuthBundle\Entity
+ * @ORM\Entity(readOnly=true)
+ */
+class User {
+    /**
+     * @var
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     */
+    private $user_id;
+}
