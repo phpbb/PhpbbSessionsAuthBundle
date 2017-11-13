2262ba5e7cdc47a058e0af1f8015cf7efa75055a
TeLiXj <telixj@gmail.com>
2017-11-13 06:40:01 +0100

    added all fields and methods

--- Entity/Session.php
+++ Entity/Session.php
@@ -22,7 +22,7 @@ class Session
 {
     /**
      * @var string
-     * @ORM\Column(type="string", name="session_id")
+     * @ORM\Column(name="session_id", type="string")
      * @ORM\Id
      */
     private $id;
@@ -35,25 +35,70 @@ class Session
     private $user;
 
     /**
-     * @var string
-     * @ORM\Column(type="string", name="session_ip")
-     *
+     * @var integer
+     * @ORM\Column(name="session_forum_id", type="integer", nullable=false)
      */
-    private $ip;
+    private $forumId;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="session_last_visit", type="integer")
+     */
+    private $lastVisit;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="session_start", type="integer")
+     */
+    private $start;
 
     /**
      * @var
-     * @ORM\Column(type="integer", name="session_time")
+     * @ORM\Column(name="session_time", type="integer")
      */
     private $time;
 
     /**
-     * autologin
-     *
+     * @var string
+     * @ORM\Column(name="session_ip", type="string")
+     */
+    private $ip;
+
+    /**
+     * @var string
+     * @ORM\Column(name="session_browser", type="string", length=150, nullable=false)
+     */
+    private $browser;
+
+    /**
+     * @var string
+     * @ORM\Column(name="session_forwarded_for", type="string", length=255, nullable=false)
+     */
+    private $forwardedFor;
+
+    /**
+     * @var string
+     * @ORM\Column(name="session_page", type="string", length=255, nullable=false)
+     */
+    private $page;
+
+    /**
      * @var boolean
-     * @ORM\Column(type="boolean", name="session_autologin")
+     * @ORM\Column(name="session_viewonline", type="boolean", nullable=false)
+     */
+    private $viewonline;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="session_autologin", type="boolean")
      */
     private $autologin;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="session_admin", type="boolean", nullable=false)
+     */
+    private $admin;
 
     /**
      * @return string
@@ -88,19 +133,58 @@ class Session
     }
 
     /**
-     * @return string
+     * @param integer $ForumId
+     * @return Sessions
      */
-    public function getIp()
+    public function setForumId($forumId)
     {
-        return $this->ip;
+        $this->forumId = $forumId;
+        return $this;
     }
 
     /**
-     * @param string $ip
+     * @return integer
      */
-    public function setIp($ip)
+    public function getForumId()
     {
-        $this->ip = $ip;
+        return $this->forumId;
+    }
+
+    /**
+     * @param integer $lastVisit
+     * @return Sessions
+     */
+    public function setLastVisit($lastVisit)
+    {
+        $this->lastVisit = $lastVisit;
+        return $this;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getLastVisit()
+    {
+        return $this->lastVisit;
+    }
+
+    /**
+     * @param integer $start
+     * @return Sessions
+     */
+    public function setStart($start)
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getStart()
+    {
+        return $this->start;
     }
 
     /**
@@ -120,8 +204,94 @@ class Session
     }
 
     /**
-     * Getter for autologin
-     *
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @param string $ip
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+    }
+
+    /**
+     * @param string $browser
+     * @return Sessions
+     */
+    public function setBrowser($browser)
+    {
+        $this->Browser = $browser;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBrowser()
+    {
+        return $this->Browser;
+    }
+
+    /**
+     * @param string $forwardedFor
+     * @return Sessions
+     */
+    public function setForwardedFor($forwardedFor)
+    {
+        $this->forwardedFor = $forwardedFor;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getForwardedFor()
+    {
+        return $this->forwardedFor;
+    }
+
+    /**
+     * @param string $page
+     * @return Sessions
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param boolean $viewonline
+     * @return Sessions
+     */
+    public function setViewonline($viewonline)
+    {
+        $this->viewonline = $viewonline;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getViewonline()
+    {
+        return $this->viewonline;
+    }
+
+    /**
      * @return boolean
      */
     public function getAutologin()
@@ -130,15 +300,30 @@ class Session
     }
 
     /**
-     * Setter for autologin
-     *
      * @param boolean $autologin
      * @return Session
      */
     public function setAutologin($autologin)
     {
         $this->autologin = $autologin;
-
         return $this;
+    }
+
+    /**
+     * @param boolean $admin
+     * @return Sessions
+     */
+    public function setAdmin($admin)
+    {
+        $this->admin = $admin;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getAdmin()
+    {
+        return $this->admin;
     }
 }
