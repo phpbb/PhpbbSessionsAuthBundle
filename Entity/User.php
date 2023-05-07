a3089346906361a5ec1d649daeafb373753596a9
TeLiXj <telixj@gmail.com>
2023-05-07 13:44:57 +0000

    removed unused method and unnecesary comments

--- Entity/User.php
+++ Entity/User.php
@@ -1055,34 +1055,11 @@ class User implements UserInterface
         return $this;
     }
 
-    /**
-     * Returns the salt that was originally used to encode the password.
-     *
-     * This can return null if the password was not encoded using a salt.
-     *
-     * @return string|null The salt
-     */
     public function getSalt(): ?string
     {
         return null;
     }
 
-    /**
-     * Returns the username used to authenticate the user.
-     *
-     * @return string The username
-     */
-    public function getName()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Removes sensitive data from the user.
-     *
-     * This is important if, at any given point, sensitive information like
-     * the plain-text password is stored on this object.
-     */
     public function eraseCredentials()
     {
     }
